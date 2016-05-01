a0952ea0a088fcaa0e6fc6c3c269e4de119dc435
jian.ma <jian.ma@dianrong.com>
2016-05-01 16:25:28 +0800

    finished the first chapter.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 def post_list(request):
 	object_list = Post.published.all()
@@ -15,6 +16,12 @@ def post_list(request):
 		# If the page is out of range, delivery the last page.
 		posts = paginator.page(paginator.num_pages)
 	return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
+class PostListView(ListView):
+	queryset = Post.published.all()
+	context_object_name = 'posts'
+	paginate_by = 3
+	template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,
